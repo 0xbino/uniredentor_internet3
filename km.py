173225a07dc8c0212db624fe069bafb5c7b37865
0xb1n0 <42788133+0xbino@users.noreply.github.com>
2019-02-27 19:57:38 -0300

    Add files via upload
    refatorando o código

--- km.py
+++ km.py
@@ -1,11 +1,13 @@
 minutos=float(input("qts min gastou: "))
+
 if minutos < 200:
     conta=minutos*0.2
-    print ("%f" %conta)
+
 elif minutos > 400:
     conta=minutos*0.15
-    print ("%f" %conta)
+
 else:
     conta=minutos*0.18
-    print ("%f" %conta)
+
+print ("%f" %conta)
     
