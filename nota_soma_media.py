5ff68d71c71fb717e1c388ef53e7280fed412cf2
0xb1n0 <42788133+0xbino@users.noreply.github.com>
2019-03-13 19:32:28 -0300

    diminuindo as linhas

--- nota_soma_media.py
+++ nota_soma_media.py
@@ -1,17 +1,11 @@
 notas = []
 i = 1
-
+soma = 0
 while i <= 4:
     n = float (input("digite: "))
     notas.append(n)
+    soma += n
     i += 1
-    
-soma = 0
-i = 0
-
-while i <= 3:
-    soma += notas[i]
-    i += 1
-    
+        
 print (notas)
 print (soma/4)
